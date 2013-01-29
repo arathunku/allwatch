303afa288b716737077becb14120d34ed440cd9b
arathunku <arathunku+git@gmail.com>
2013-01-29 21:24:26 +0100

    Init

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AllwatchSite::Application.config.secret_token = 'd1e49c488ca134bfd022c28c41690f26be79f9b43969e0c64eba1da854b5789041809d20ab952723693d66e3c4a67484063444d2343cb60675c32087f1f877ff'
