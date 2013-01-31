80be22fd99693bf23801462f09a5024d46e6ea0f
arathunku <arathunku+git@gmail.com>
2013-01-31 23:57:42 +0100

    Signup/in/out added

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,12 @@
 AllwatchSite::Application.routes.draw do
   root to: 'static_page#home'
   resources :users
+  resources :sessions, only: [:new, :create, :destroy]
 
   match "/signup",     to: "users#new"
+  match '/signin',     to: "sessions#new"
+  match '/signout',    to: "sessions#destroy", via: :delete
+  
   match '/blog/about', to: "static_page#about"
+
 end
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -21,13 +21,20 @@ describe User do
   it { should respond_to(:password_digest) }
   it { should respond_to(:password) }
   it { should respond_to(:password_confirmation) }
+  it { should respond_to(:remember_token) }
   it { should respond_to(:authenticate) }
   it { should be_valid }
+
+  describe "remember token" do
+    before { @user.save }
+    its(:remember_token) { should_not be_blank }
+  end
 
   describe "email not present " do
     before { @user.email = "" }
     it { should_not be_valid }
   end
+
   describe "email too long" do
     before { @user.email = "a"*129}
     it { should_not be_valid}
